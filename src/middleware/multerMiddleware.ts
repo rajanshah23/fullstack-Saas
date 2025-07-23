615c24dbfd3f4d82f4b353c7d02168531dbca7ff
rajanshah23 <shahrajan774@gmail.com>
2025-07-23 17:58:09 +0545

    multer configuration

---
+++ src/middleware/multerMiddleware.ts
@@ -0,0 +1,15 @@
+import { Request } from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  //Yo ho location   incoming file  lai kaha rakhney
+  ///cb--->callback function--------------cb(error,success)
+  destination: function (req: Request, file: Express.Multer.File, cb: any) {
+    cb(null, "./src/storage");
+  },
+  //jun location ma file save vaxaa tesko name k rakhney
+  filename: function (req: Request, file: Express.Multer.File, cb: any) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+export default { multer, storage };
